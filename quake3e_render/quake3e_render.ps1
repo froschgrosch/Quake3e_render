f967937de33e47e9056f9c7b13d05f4e4473520f
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2022-03-08 22:43:29 +0100

    Replace aliases by cmdlet names and remove old comments

--- quake3e_render/quake3e_render.ps1
+++ quake3e_render/quake3e_render.ps1
@@ -1,47 +1,43 @@
 # Powershell application to aid in demo rendering.
 # https://github.com/froschgrosch/quake3-scripts
 
-# Add-Type -AssemblyName System.Windows.Forms
-
-
-$config = Get-Content .\zz_render\config.json.txt | ConvertFrom-Json
+$config = Get-Content .\zz_render\config.json | ConvertFrom-Json
 
 $currentDuration = 0
-
 $outputPath = $config.application.outputPath
 
+# === APPLICATION === 
 
 if ($mergeRender){
     Remove-Item .\zz_render\merge_rendertemp\*.mp4
     
     if (Test-Path -PathType Leaf "$outputPath\merge_demolist.txt"){ Remove-Item "$outputPath\merge_demolist.txt" }
 
-    echo "ffconcat version 1.0" | Out-File -Encoding ascii .\zz_render\mergerenderlist.txt
+    Write-Output "ffconcat version 1.0" | Out-File -Encoding ascii .\zz_render\mergerenderlist.txt
 }
 
 
 :demoLoop foreach($file in $(Get-ChildItem .\render_input\ | Sort-Object -Property LastWriteTime)){
 
     $demoName = $file.Name.Remove($file.Name.Length - 6, 6)
-    echo "Demo: $demoName"
+    Write-Output "Demo: $demoName"
 
     # Skip conditions
     
     foreach ($skipKeyword in $config.application.skipKeywords){
-        #echo "checking for $skipKeyword"
+        #Write-Output "checking for $skipKeyword"
         if ( $file.Name.StartsWith($skipKeyword) ){
-            echo "Contains ""$skipKeyword"", skipping..." " "
+            Write-Output "Contains ""$skipKeyword"", skipping..." " "
             continue demoLoop
         }
     }
   
     if ($(Test-Path -PathType Leaf "$outputPath\$demoName.mp4") -and -not $config.user.mergeRender) {
 
-        #$msgboxResult = [System.Windows.MessageBox]::Show("This demo was already rendered at some point. Would you like to render again?","Info",4,32)
         do { $msgboxResult = Read-Host "This demo was already rendered at some point. Would you like to render again? (y/n)"} while(-not @("y","n").Contains($msgboxResult))
 
         if ($msgboxResult -eq "n"){
-			echo " "
+			Write-Output " "
             continue demoLoop
         } else {
             Remove-Item "$outputPath\$demoName.mp4"
@@ -52,20 +48,20 @@ if ($mergeRender){
     $game = $demoData.gameStates[0].configStringValues.fs_game
 
     if (-not  $file.Extension -eq ".dm_68" -or -not $config.application.validGames.Contains($game)){
-        echo "Not a valid demo, skipping..." " "
+        Write-Output "Not a valid demo, skipping..." " "
         continue demoLoop
     }
 
     
     # Render
-    $captureName = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 11 | % {[char]$_})
+    $captureName = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 11 | ForEach-Object {[char]$_})
 	
     if ($config.user.mergeRender) {
-        echo "file merge_rendertemp/$captureName.mp4" | Out-File -Append -Encoding ascii .\zz_render\mergerenderlist.txt
+        Write-Output "file merge_rendertemp/$captureName.mp4" | Out-File -Append -Encoding ascii .\zz_render\mergerenderlist.txt
     }
 
 
-    echo "Rendering... (capturename: $captureName)"
+    Write-Output "Rendering... (capturename: $captureName)"
 
     Copy-Item ".\render_input\$demoName.dm_68" ".\$game\demos\$captureName.dm_68"
     
@@ -87,14 +83,14 @@ if ($mergeRender){
     $mins = $(Measure-Command {Wait-Process -InputObject $q3e_proc}).TotalMinutes
 
 
-    echo "Time in minutes: $mins" " "
-    sleep 3
+    Write-Output "Time in minutes: $mins" " "
+    Start-Sleep 3
     
     if ($config.user.mergeRender){
         $ffprobeData = $(ffprobe -v error -hide_banner -of json -show_entries format ".\$game\videos\$captureName.mp4") | ConvertFrom-Json
         $timestamp = $("{0:hh\:mm\:ss}" -f $([timespan]::fromseconds($currentDuration)))
 
-        echo "$timestamp $demoName" | Out-File -Append .\zz_render\merge_demolist.txt
+        Write-Output "$timestamp $demoName" | Out-File -Append .\zz_render\merge_demolist.txt
 
         $currentDuration += $ffprobeData.format.duration
 
@@ -114,13 +110,13 @@ if ($mergeRender){
     Remove-Item ".\$game\demos\$captureName.dm_68"
     #Remove-Item ".\render_input\$demoName.dm_68"
 
-    sleep 3
+    Start-Sleep 3
 }
 
 # Merge with ffmpeg
 
 if ($config.user.mergeRender){
-    echo "Merging with ffmpeg..."
+    Write-Output "Merging with ffmpeg..."
     .\ffmpeg.exe -y -f concat -safe 0 -i zz_render\mergerenderlist.txt -c copy "$outputPath\merge_output.mp4"
     
     Move-Item .\zz_render\merge_demolist.txt "$outputPath\merge_demolist.txt"
@@ -128,7 +124,7 @@ if ($config.user.mergeRender){
     Remove-Item .\zz_render\merge_rendertemp\*.mp4
 }
 
-echo "Rendering finished."
+Write-Output "Rendering finished."
 
 #shutdown -s -t 60
 pause
