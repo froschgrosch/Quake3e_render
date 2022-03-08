3baaa866275c65c44c3527e7d0b4a3ce2d9c06e1
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2022-03-08 18:05:31 +0100

    Convert CRLF to LF

--- quake3e_render/quake3e_render.ps1
+++ quake3e_render/quake3e_render.ps1
@@ -11,7 +11,8 @@ $currentDuration = 0
 $outputPath = $config.application.outputPath
 
 
-if ($mergeRender){    Remove-Item .\zz_render\merge_rendertemp\*.mp4
+if ($mergeRender){
+    Remove-Item .\zz_render\merge_rendertemp\*.mp4
     
     if (Test-Path -PathType Leaf "$outputPath\merge_demolist.txt"){ Remove-Item "$outputPath\merge_demolist.txt" }
 
@@ -67,6 +68,67 @@ if ($mergeRender){    Remove-Item .\zz_render\merge_rendertemp\*.mp4
     echo "Rendering... (capturename: $captureName)"
 
     Copy-Item ".\render_input\$demoName.dm_68" ".\$game\demos\$captureName.dm_68"
-        $q3e_args = @(        "+set fs_game $game",        "+set nextdemo quit",        "+seta in_nograb 1",        "+seta r_renderScale " +    $config.user.renderScale.enabled        "+seta r_renderWidth " +    $config.user.renderScale.resolution[0],        "+seta r_renderHeight " +   $config.user.renderScale.resolution[1],        "+seta cl_aviPipeFormat " + $config.application.ffmpegPipeFormats[$config.user.ffmpegMode],        "+seta cl_aviFrameRate " +  $config.user.framerate,        "+demo $captureName",        "+video-pipe $captureName"    )    $q3e_proc = Start-Process -PassThru -ArgumentList $q3e_args -FilePath .\quake3e.x64.exe    $mins = $(Measure-Command {Wait-Process -InputObject $q3e_proc}).TotalMinutes    echo "Time in minutes: $mins" " "    sleep 3        if ($config.user.mergeRender){        $ffprobeData = $(ffprobe -v error -hide_banner -of json -show_entries format ".\$game\videos\$captureName.mp4") | ConvertFrom-Json        $timestamp = $("{0:hh\:mm\:ss}" -f $([timespan]::fromseconds($currentDuration)))        echo "$timestamp $demoName" | Out-File -Append .\zz_render\merge_demolist.txt        $currentDuration += $ffprobeData.format.duration        if (Test-Path -PathType Leaf "zz_render\merge_rendertemp\$captureName.mp4"){ Remove-Item  "zz_render\merge_rendertemp\$captureName.mp4" }        Move-Item -Force ".\$game\videos\$captureName.mp4" "zz_render\merge_rendertemp\$captureName.mp4"    } else {        Move-Item -Force ".\$game\videos\$captureName.mp4" "$outputPath\$demoName.mp4"    }        if($config.user.keepLog -and -not $config.user.mergeRender){        Move-Item -Force ".\$game\videos\$captureName.mp4-log.txt" "$outputPath\$demoName.log"    } else {        Remove-Item ".\$game\videos\$captureName.mp4-log.txt"    }    Remove-Item ".\$game\demos\$captureName.dm_68"    #Remove-Item ".\render_input\$demoName.dm_68"    sleep 3}# Merge with ffmpegif ($config.user.mergeRender){    echo "Merging with ffmpeg..."
+    
+    $q3e_args = @(
+        "+set fs_game $game",
+        "+set nextdemo quit",
+        "+seta in_nograb 1",
+        "+seta r_renderScale " +    $config.user.renderScale.enabled
+        "+seta r_renderWidth " +    $config.user.renderScale.resolution[0],
+        "+seta r_renderHeight " +   $config.user.renderScale.resolution[1],
+        "+seta cl_aviPipeFormat " + $config.application.ffmpegPipeFormats[$config.user.ffmpegMode],
+        "+seta cl_aviFrameRate " +  $config.user.framerate,
+        "+demo $captureName",
+        "+video-pipe $captureName"
+    )
+
+    $q3e_proc = Start-Process -PassThru -ArgumentList $q3e_args -FilePath .\quake3e.x64.exe
+
+    $mins = $(Measure-Command {Wait-Process -InputObject $q3e_proc}).TotalMinutes
+
+
+    echo "Time in minutes: $mins" " "
+    sleep 3
+    
+    if ($config.user.mergeRender){
+        $ffprobeData = $(ffprobe -v error -hide_banner -of json -show_entries format ".\$game\videos\$captureName.mp4") | ConvertFrom-Json
+        $timestamp = $("{0:hh\:mm\:ss}" -f $([timespan]::fromseconds($currentDuration)))
+
+        echo "$timestamp $demoName" | Out-File -Append .\zz_render\merge_demolist.txt
+
+        $currentDuration += $ffprobeData.format.duration
+
+        if (Test-Path -PathType Leaf "zz_render\merge_rendertemp\$captureName.mp4"){ Remove-Item  "zz_render\merge_rendertemp\$captureName.mp4" }
+        Move-Item -Force ".\$game\videos\$captureName.mp4" "zz_render\merge_rendertemp\$captureName.mp4"
+
+    } else {
+        Move-Item -Force ".\$game\videos\$captureName.mp4" "$outputPath\$demoName.mp4"
+    }
+    
+    if($config.user.keepLog -and -not $config.user.mergeRender){
+        Move-Item -Force ".\$game\videos\$captureName.mp4-log.txt" "$outputPath\$demoName.log"
+    } else {
+        Remove-Item ".\$game\videos\$captureName.mp4-log.txt"
+    }
+
+    Remove-Item ".\$game\demos\$captureName.dm_68"
+    #Remove-Item ".\render_input\$demoName.dm_68"
+
+    sleep 3
+}
+
+# Merge with ffmpeg
+
+if ($config.user.mergeRender){
+    echo "Merging with ffmpeg..."
     .\ffmpeg.exe -y -f concat -safe 0 -i zz_render\mergerenderlist.txt -c copy "$outputPath\merge_output.mp4"
-        Move-Item .\zz_render\merge_demolist.txt "$outputPath\merge_demolist.txt"    Remove-Item .\zz_render\mergerenderlist.txt    Remove-Item .\zz_render\merge_rendertemp\*.mp4}echo "Rendering finished."#shutdown -s -t 60pause
+    
+    Move-Item .\zz_render\merge_demolist.txt "$outputPath\merge_demolist.txt"
+    Remove-Item .\zz_render\mergerenderlist.txt
+    Remove-Item .\zz_render\merge_rendertemp\*.mp4
+}
+
+echo "Rendering finished."
+
+#shutdown -s -t 60
+pause
