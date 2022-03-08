f0647c7891bfce82c22ed12ca7fd207352a46cbb
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2022-03-08 18:00:41 +0100

    Various updates
    Move configuration to separate json file;
    Pass the appropiateprocess object to the wait-process cmdlet;

--- quake3e_render/README.md
+++ quake3e_render/README.md
@@ -4,30 +4,31 @@ A Powershell application to aid in demo rendering. Run with *Right-click\Run wit
 
 ## Installation.
 
-Put the script file and a recent version of [ffmpeg](http://ffmpeg.org/download.html) and [quake3e](https://github.com/ec-/Quake3e/releases/tag/latest) in your quake3 folder. Create the directories *render_input*, *render_output* and *zz_tools*. Inside the *zz_tools* folder, create a folder *merge_rendetemp*. Download [myt's UberDemoTools](https://github.com/mightycow/uberdemotools) for command line, and put UDT_json.exe in the *zz_tools* folder you just created. 
-Now it is time to configure the application.
+Put the script file and a recent version of [ffmpeg](http://ffmpeg.org/download.html) and [quake3e](https://github.com/ec-/Quake3e/releases/tag/latest) in your quake3 folder. Create the directories *render_input*, *render_output* and *zz_render*. Inside the *zz_render* folder, create a folder *merge_rendetemp* and a folder *tools*. Download [myt's UberDemoTools](https://github.com/mightycow/uberdemotools) for command line, and put UDT_json.exe in the *zz_render/tools* folder you just created. 
+
+Now it is time to configure the application. To do that, you edit *config.json.txt* in *zz_render*.
 
 ## User configurable params
 
 ```
-$mergeRender = 0 # ( 1 | 0 )          # When set to 1, all rendered demos will be merged into one videofile with ffmpeg.
+mergeRender = 0 # ( 1 | 0 )          # When set to 1, all rendered demos will be merged into one videofile with ffmpeg.
 
-$renderScale = 0 # ( 1 | 0 )          # When set to 1, the cvar r_renderScale will be enabled. It enables rendering at resolutions greater than screen resolution.
-$renderResolution = @("3840","2160")  # Set render width and height respectively.
-$framerate = 60                       # Set the output framerate.
+renderScale = 0 # ( 1 | 0 )          # When set to 1, the cvar r_renderScale will be enabled. It enables rendering at resolutions greater than screen resolution.
+renderResolution = @("3840","2160")  # Set render width and height respectively.
+framerate = 60                       # Set the output framerate.
 
-$ffmpegMode = 0                       # Select index of the ffmpeg pipe format mode, which is stored in the $ffmpegModes variable. 
+ffmpegMode = 0                       # Select index of the ffmpeg pipe format mode, which is stored in the $ffmpegModes variable. 
 
-$keepLog = 0 # ( 1 | 0 )              # When set to 1, the ffmpeg encoding logs will be copied to the output path.
+keepLog = 0 # ( 1 | 0 )              # When set to 1, the ffmpeg encoding logs will be copied to the output path.
 ```
 
 ## Application configuration
 
 ```
-$outputPath = ".\render_output"     # The path where the script will put finished video files. The path can be absolute or relative to your quake3 installation.
-$skipKeywords = @("SKIP")           # If the demo starts with one of these keywords, it will be skipped in rendering. Useful if you don't want to render a demo twice.
-$validGames = @("baseq3")           # Put a list of the installed mods here. If the demo's fs_game isn't in here, it will be skipped.
+outputPath = ".\render_output"     # The path where the script will put finished video files. The path can be absolute or relative to your quake3 installation.
+skipKeywords = @("SKIP")           # If the demo starts with one of these keywords, it will be skipped in rendering. Useful if you don't want to render a demo twice.
+validGames = @("baseq3")           # Put a list of the installed mods here. If the demo's fs_game isn't in here, it will be skipped.
 
-$ffmpegModes = @("-preset medium -crf 23 -vcodec libx264 -flags +cgop -pix_fmt yuv420 -bf 2 -codec:a aac -strict -2 -b:a 160k -r:a 22050 -movflags faststart")
+ffmpegModes = @("-preset medium -crf 23 -vcodec libx264 -flags +cgop -pix_fmt yuv420 -bf 2 -codec:a aac -strict -2 -b:a 160k -r:a 22050 -movflags faststart")
 # Here the different ffmpeg configurations are stored. You can put one for sofware rendering and one for hardware rendering, for example. 
 ```
--- quake3e_render/quake3e_render.ps1
+++ quake3e_render/quake3e_render.ps1
@@ -1,43 +1,23 @@
 # Powershell application to aid in demo rendering.
-# github/froschgrosch/quake3-scripts
-
-# === USER CONFIG ===
-
-$mergeRender = 0
-
-$renderScale = 0
-$renderResolution = @("3840","2160")
-$framerate = 60
-
-$ffmpegMode = 0
-
-$keepLog = 0
-
-# === APPLICATION CONFIG ===
-
-$outputPath = ".\render_output" # no trailing backslash 
-
-$skipKeywords=@("SKIP")
-$validGames = @("baseq3")
-
-$ffmpegModes = @(
-    """-pix_fmt yuv420p -c:v libx264  -crf 23  -profile:v high -preset:v medium -maxrate:v $ffmpegBitrate -bufsize 2M -bf 2 -c:a aac -strict -2 -b:a 384k -r:a 48k -movflags faststart"""  # software h264 1080p crf23
-)
-
-
-# === APPLICATION ===
+# github/froschgrosch/quake3-powershell-stuff
 
 # Add-Type -AssemblyName System.Windows.Forms
 
-if ($mergeRender){
-    Remove-Item ".\zz_tools\merge_rendertemp\*.mp4"
-    
-    if (Test-Path -PathType Leaf "$outputPath\merge_demolist.txt"){
-        Remove-Item "$outputPath\merge_demolist.txt"
-    }
 
-    echo "ffconcat version 1.0" | Out-File -Encoding ascii .\zz_tools\mergerenderlist.txt
+$config = Get-Content .\zz_render\config.json.txt | ConvertFrom-Json
+
+$currentDuration = 0
+
+$outputPath = $config.application.outputPath
+
+
+if ($mergeRender){    Remove-Item .\zz_render\merge_rendertemp\*.mp4
+    
+    if (Test-Path -PathType Leaf "$outputPath\merge_demolist.txt"){ Remove-Item "$outputPath\merge_demolist.txt" }
+
+    echo "ffconcat version 1.0" | Out-File -Encoding ascii .\zz_render\mergerenderlist.txt
 }
+
 
 :demoLoop foreach($file in $(Get-ChildItem .\render_input\ | Sort-Object -Property LastWriteTime)){
 
@@ -46,14 +26,15 @@ if ($mergeRender){
 
     # Skip conditions
     
-    foreach ($skipKeyword in $skipKeywords){
+    foreach ($skipKeyword in $config.application.skipKeywords){
+        #echo "checking for $skipKeyword"
         if ( $file.Name.StartsWith($skipKeyword) ){
             echo "Contains ""$skipKeyword"", skipping..." " "
             continue demoLoop
         }
     }
   
-    if ($(Test-Path -PathType Leaf "$outputPath\$demoName.mp4") -and -not $mergeRender) {
+    if ($(Test-Path -PathType Leaf "$outputPath\$demoName.mp4") -and -not $config.user.mergeRender) {
 
         #$msgboxResult = [System.Windows.MessageBox]::Show("This demo was already rendered at some point. Would you like to render again?","Info",4,32)
         do { $msgboxResult = Read-Host "This demo was already rendered at some point. Would you like to render again? (y/n)"} while(-not @("y","n").Contains($msgboxResult))
@@ -66,30 +47,26 @@ if ($mergeRender){
         }
     }
 
-    $demoData = .\zz_tools\UDT_json.exe -c "..\render_input\$demoName.dm_68" | ConvertFrom-Json
+    $demoData = .\zz_render\tools\UDT_json.exe -c "..\..\render_input\$demoName.dm_68" | ConvertFrom-Json
     $game = $demoData.gameStates[0].configStringValues.fs_game
 
-    if (-not  $file.name.Substring($file.Name.Length - 6, 6) -eq ".dm_68" -or -not $validGames.Contains($game)){
+    if (-not  $file.Extension -eq ".dm_68" -or -not $config.application.validGames.Contains($game)){
         echo "Not a valid demo, skipping..." " "
         continue demoLoop
     }
 
     
-    
     # Render
-
     $captureName = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 11 | % {[char]$_})
 	
-    if ($mergeRender) {
-        echo "file merge_rendertemp/$captureName.mp4" | Out-File -Append -Encoding ascii .\zz_tools\mergerenderlist.txt
-        echo "Demo $demoName" | Out-File -Append .\zz_tools\merge_demolist.txt
+    if ($config.user.mergeRender) {
+        echo "file merge_rendertemp/$captureName.mp4" | Out-File -Append -Encoding ascii .\zz_render\mergerenderlist.txt
     }
 
 
     echo "Rendering... (capturename: $captureName)"
 
     Copy-Item ".\render_input\$demoName.dm_68" ".\$game\demos\$captureName.dm_68"
-        .\quake3e.x64.exe `    +set fs_game $game `    +set nextdemo "quit" `
-	+seta in_nograb 1 `    +seta r_renderScale $renderScale `    +seta r_renderWidth $renderResolution[0] `    +seta r_renderHeight $renderResolution[1] `    +seta cl_aviPipeFormat $ffmpegModes[$ffmpegMode] `    +seta cl_aviFrameRate $framerate `    +demo $captureName +video-pipe $captureName        $mins = $(Measure-Command {Wait-Process -name "quake3e.x64"}).TotalMinutes    echo "Time in minutes: $mins" " "    sleep 3        if ($mergeRender){        if (Test-Path -PathType Leaf "zz_tools\merge_rendertemp\$captureName.mp4"){            Remove-Item  "zz_tools\merge_rendertemp\$captureName.mp4"        }        Move-Item -Force ".\$game\videos\$captureName.mp4" "zz_tools\merge_rendertemp\$captureName.mp4"    } else {        Move-Item -Force ".\$game\videos\$captureName.mp4" "$outputPath\$demoName.mp4"    }        if($keepLog -and -not $mergeRender){        Move-Item -Force ".\$game\videos\$captureName.mp4-log.txt" "$outputPath\$demoName.log"    } else {        Remove-Item ".\$game\videos\$captureName.mp4-log.txt"    }    Remove-Item ".\$game\demos\$captureName.dm_68"    sleep 3}# Merge with ffmpegif ($mergeRender){    echo "Merging with ffmpeg..."
-    .\ffmpeg.exe -v 0 -y -f concat -safe 0 -i zz_tools\mergerenderlist.txt -c copy "$outputPath\merge_output.mp4"
-        Move-Item .\zz_tools\merge_demolist.txt "$outputPath\merge_demolist.txt"    Remove-Item .\zz_tools\mergerenderlist.txt    Remove-Item .\zz_tools\merge_rendertemp\*.mp4}echo "Rendering finished."#shutdown -s -t 60pause
+        $q3e_args = @(        "+set fs_game $game",        "+set nextdemo quit",        "+seta in_nograb 1",        "+seta r_renderScale " +    $config.user.renderScale.enabled        "+seta r_renderWidth " +    $config.user.renderScale.resolution[0],        "+seta r_renderHeight " +   $config.user.renderScale.resolution[1],        "+seta cl_aviPipeFormat " + $config.application.ffmpegPipeFormats[$config.user.ffmpegMode],        "+seta cl_aviFrameRate " +  $config.user.framerate,        "+demo $captureName",        "+video-pipe $captureName"    )    $q3e_proc = Start-Process -PassThru -ArgumentList $q3e_args -FilePath .\quake3e.x64.exe    $mins = $(Measure-Command {Wait-Process -InputObject $q3e_proc}).TotalMinutes    echo "Time in minutes: $mins" " "    sleep 3        if ($config.user.mergeRender){        $ffprobeData = $(ffprobe -v error -hide_banner -of json -show_entries format ".\$game\videos\$captureName.mp4") | ConvertFrom-Json        $timestamp = $("{0:hh\:mm\:ss}" -f $([timespan]::fromseconds($currentDuration)))        echo "$timestamp $demoName" | Out-File -Append .\zz_render\merge_demolist.txt        $currentDuration += $ffprobeData.format.duration        if (Test-Path -PathType Leaf "zz_render\merge_rendertemp\$captureName.mp4"){ Remove-Item  "zz_render\merge_rendertemp\$captureName.mp4" }        Move-Item -Force ".\$game\videos\$captureName.mp4" "zz_render\merge_rendertemp\$captureName.mp4"    } else {        Move-Item -Force ".\$game\videos\$captureName.mp4" "$outputPath\$demoName.mp4"    }        if($config.user.keepLog -and -not $config.user.mergeRender){        Move-Item -Force ".\$game\videos\$captureName.mp4-log.txt" "$outputPath\$demoName.log"    } else {        Remove-Item ".\$game\videos\$captureName.mp4-log.txt"    }    Remove-Item ".\$game\demos\$captureName.dm_68"    #Remove-Item ".\render_input\$demoName.dm_68"    sleep 3}# Merge with ffmpegif ($config.user.mergeRender){    echo "Merging with ffmpeg..."
+    .\ffmpeg.exe -y -f concat -safe 0 -i zz_render\mergerenderlist.txt -c copy "$outputPath\merge_output.mp4"
+        Move-Item .\zz_render\merge_demolist.txt "$outputPath\merge_demolist.txt"    Remove-Item .\zz_render\mergerenderlist.txt    Remove-Item .\zz_render\merge_rendertemp\*.mp4}echo "Rendering finished."#shutdown -s -t 60pause
