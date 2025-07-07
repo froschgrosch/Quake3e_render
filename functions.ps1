62d92437e89aed568a63e00abedb68ac3b8f0f96
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2025-07-07 22:00:54 +0200

    Create functions.ps1

---
+++ functions.ps1
@@ -0,0 +1,19 @@
+### Functions ###
+function Get-UserConfirmation($prompt){ # Do a yes/no query and return $true/$false 
+    do { 
+        $msgboxResult = Read-Host "$prompt (y/n)"
+    } while(-not @('y','n').Contains($msgboxResult))
+
+    return $msgboxResult -eq 'y'
+}
+function Read-Json ($inputPath) {
+    return Get-Content $inputPath | ConvertFrom-Json
+}
+
+function Add-ToObject ($inputObject, $name, $value) {
+    Add-Member -Force -InputObject $inputObject -MemberType NoteProperty -Name $name -Value $value
+}
+
+function Add-NewProperty ($inputObject, $name){
+    Add-ToObject $inputObject -name $name -value (New-Object -TypeName 'PSObject')
+}
